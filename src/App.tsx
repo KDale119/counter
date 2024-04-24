76e5ae7764c789bd788355511ccbe0b8f7962d8e
Kayla Dale <kayla.dale@mutualofomaha.com>
2024-04-23 21:32:31 -0500

    added styling & add functionality of counter with exception of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./components/Counter";
 
 function App() {
   return (
       <div className="App">
-          <h2 className="header">Counter</h2>
-          <button className="button">Increment</button>
-          <button className="button">Decrement</button>
-
-          <div className="currCount">
-              Current Count:
-          </div>
+          <Counter />
       </div>
   );
 }
